cbe79a00f91c5d31034ef4699f9a14322e653c8a
Nick Casciola <s0948644@monmouth.edu>
2017-10-17 19:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 
 var katzDeli = [];
 
- function takeANumber(katzDeli, name) {
-   katzDeli.push(name);
-     return('Welcome, ' + name + '. You are number ' +katzDeli.length +' in line.');
- }
-
-
  function nowServing(katzDeli) {
    var i = 0;
   while (i < katzDeli.length) {
